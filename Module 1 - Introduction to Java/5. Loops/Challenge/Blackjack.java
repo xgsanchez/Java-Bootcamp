eb52278ba5b4ed0579674843b221aa9cc0651b19
Xiara Garcia <xgarcia@hacienda.gov.dom>
2025-05-14 13:49:37 -0400

    refactorizacion black jack

--- Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
+++ Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
@@ -13,34 +13,27 @@ public class Blackjack {
 
         int valueCard1 = drawRandomCard();
         int valueCard2 = drawRandomCard();
-
-        String card1 = cardString(valueCard1);
-        String card2 = cardString(valueCard2);
-
-        System.out.println("\n You get a \n" + card1 + "\nand a \n" + card2);
-
+        System.out.println("\n You get a \n" + cardString(valueCard1) + "\nand a \n" + cardString(valueCard2));
 
         int yourTotal = Math.min(valueCard1, 10) + Math.min(valueCard2, 10);
         System.out.println("Your total is " + yourTotal);
-        
-
 
         int valueCardComputer1 = drawRandomCard();
         int valueCardComputer2 = drawRandomCard();
-        String cardComputer1 = cardString(valueCardComputer1);
-        String cardComputer2 = cardString(valueCardComputer2);
 
         int computerTotal = Math.min(valueCardComputer1, 10) + Math.min(valueCardComputer2, 10);
-        System.out.println("\nThe dealer show \n" + cardComputer1 + "\\and has a card facing down \\n" + faceDown());
+        System.out.println("\nThe dealer show \n" + cardString(valueCardComputer1) + "\\and has a card facing down \\n" + faceDown());
 
         System.out.println("\nThe dealer's total is hidden\n");
 
         while (true){
             String answer = hitOrStay();
-            if (answer.equalsIgnoreCase("hit")){
+            if (answer.equals("stay")){
+                break;
+            }
+
                 int valueCard3 = drawRandomCard();
-                String card = cardString(valueCard3);
-                System.out.println("\nYou get a \n" + card);
+                System.out.println("\nYou get a \n" + cardString(valueCard3));
                 yourTotal += Math.min(valueCard3,10);
 
                 System.out.println("Your new total is " + yourTotal);
@@ -49,20 +42,15 @@ public class Blackjack {
                     System.out.println("Bust! Player loses");
                     System.exit(0);
                 }
-            }else {
-                break;
-            }
         }
 
-        System.out.println("the Dealer's card is\n" + cardComputer1 + "\nand a \n" + cardComputer2);
+        System.out.println("the Dealer's card is\n" + cardString(valueCardComputer1) + "\nand a \n" + cardString(valueCardComputer2));
         System.out.println("Dealer's total is " + computerTotal);
 
-        while(true){
-            if (computerTotal<=17){
+        while(computerTotal<=17){
 
                 int valueCardComputer3 = drawRandomCard();
-                String card = cardString(valueCardComputer3);
-                System.out.println("\nDealer gets a \n" + card);
+                System.out.println("\nDealer gets a \n" + cardString(valueCardComputer3));
                 computerTotal += Math.min(valueCardComputer3, 10);
                 System.out.println("Dealer's total is " + computerTotal);
 
@@ -70,10 +58,7 @@ public class Blackjack {
                     System.out.println("Bust! Dealer loses");
                     System.exit(0);
                 }
-            }else {
-                break;
             }
-        }
 
         if (yourTotal > computerTotal){
             System.out.println("You win");
@@ -81,35 +66,13 @@ public class Blackjack {
             System.out.println("Dealer wins");
         }
          scan.close();
-
     }
-
-
-
-
-
-    /** Task 1 – make a function that returns a random number between 1 and 13
-     * Function name – drawRandomCard
-     * @return (int)
-     *
-     * Inside the function:
-     *   1. Gets a random number between 1 and 13.
-     *   2. Returns a card.
-     */
 
     public static int drawRandomCard(){
         double random = Math.random() * 13 +1;
         return (int)random;
     }
 
-    /** Task 2 – make a function that returns a String drawing of the card.
-     * Function name – cardString
-     * @param cardNumber (int)
-     * @return (String)
-     *
-     * Inside the function:
-     *   1. Returns a String drawing of the card.
-     */
     public static String cardString(int cardNumber){
         switch (cardNumber){
            case 1: return
@@ -226,7 +189,6 @@ public class Blackjack {
         return "si llego aqui, hay un fallo ";
     }
 
-
     public static String faceDown() {
         return
         "   _____\n"+
@@ -236,17 +198,6 @@ public class Blackjack {
         "  |  J  |\n"+
         "  |_____|\n";
     }
-    
-    /** Task 7 – make a function that asks the user to hit or stay.
-     * Function name – hitOrStay
-     * @return (String)
-     *
-     * Inside the function:
-     *   1. Asks the user to hit or stay.
-     *   2. If the user doesn't enter "hit" or "stay", keep asking them to try again by printing:
-     *      Please write 'hit' or 'stay'
-     *   3. Returns the user's option 
-     */
 
     public static String hitOrStay() {
 
@@ -259,9 +210,5 @@ public class Blackjack {
         }
         return answer;
     }
-
-
-
-
 }
 
